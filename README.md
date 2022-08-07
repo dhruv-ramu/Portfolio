ef7b9c6aa71e368d13649b7312b4446dc6edfb62
Dhruvsr <dhruvramu@gmail.com>
2022-08-07 18:42:43 +0530

    README updated

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 
 <br/>
 
-<center>
-This is my portfolio website
-</center>
+### This is my portfolio website - the tools I used to build this are
+- HTML5
+- CSS
+- ReactJS
+
+### Credits
+- Amplify UI: https://ui.docs.amplify.aws/
+- React Bootstrap: https://react-bootstrap.github.io/
+- Loading SVG: https://github.com/SamHerbert/SVG-Loaders
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,15 +82,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            {/* <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item> */}
+           
 
             <Nav.Item>
               <Nav.Link
